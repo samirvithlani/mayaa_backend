7c712a0e60019a88f001b3312083aa8c9a21663d
tejas-lohar <125259557+tejas-lohar@users.noreply.github.com>
2025-12-25 14:53:31 +0530

    Update afterInstall.bash

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -9,4 +9,4 @@ docker rm $(docker ps -a -q --filter "publish=5000")
 docker ps -a -f status=created -q | xargs -r docker rm
 docker ps -a -f status=exited -q | xargs -r docker rm
 docker images --filter "dangling=true" -q | xargs -r docker rmi
-docker run -d -p 5000:5000 mayakids-backend:1.0
+docker run -d -p 3000:3000 mayakids-backend:1.0
